a2ecce42f109415e35ea2236875598e7a9bb13ce
Geovanny <sgmichel94@gmail.com>
2024-03-22 12:33:31 -0600

    quinto ajuste

--- src/components/Componente2.jsx
+++ src/components/Componente2.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import qr from "../assets/qr.png";
 
 const Componente2 = () => {
   return (
 
     <div className='bg-white flex flex-col '>
 
-        <div className='w-2/3 bg-blue-500 mx-auto my-5 p-10 space-y-5'>
+        <div className=' w-2/3 bg-blue-500 mx-auto my-5 p-10 space-y-5'>
             <h1 className='text-2xl font-bold'>Lorem ipsum dolor,niam natus?</h1>
 
             <p className='text-white text-lg'>rem ipsum dolor,niam natu</p>
@@ -17,12 +18,20 @@ const Componente2 = () => {
 
         </div>
 
-        <div className='w-full pl-36 py-12 my-2 bg-blue-200'>
+        <div className='w-full pl-36 py-12 my-2 bg-blue-200 flex justify-center gap-96 '>
+          <div>
             <h2 className='font-bold text-2xl'>¿Lorem iere  Aliquam, beatae?</h2>
 
-            <p className=' text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt sequi explicabo illum sapiente!</p>
+            <p className=' text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt!</p>
 
             <p className='text-lg'>investiga como hacerlo</p>
+
+          </div>
+            
+
+            <div>
+              <img src={qr} alt="" />
+            </div>
 
         </div>
     </div>
--- src/components/Slider2.jsx
+++ src/components/Slider2.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import ReactPlayer from 'react-player'
 
 import Navegacion from "./Navegacion";
 
@@ -21,6 +22,7 @@ const [currentSlide, setCurrentSlide] = useState(0);
     { id: 2,   },
     { id: 3,   },
     { id: 4,   },
+    { id: 5,   },
     
   ];
 
@@ -42,29 +44,31 @@ const [currentSlide, setCurrentSlide] = useState(0);
 
     <div className='slider-container'>
 
-        <div className="slider" style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
+          <div className="slider" style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
 
-        <div className="slide grid grid-cols-2 ">
+            <div className="slide grid grid-cols-2 ">
 
-          <div className="degradado w-full h-full flex flex-col justify-center items-center space-y-7">
+              <div className="degradado w-full h-full flex flex-col justify-center items-center space-y-7">
 
-            <h2 className='text-center font-extrabold text-white uppercase text-5xl'>Factoraje sostenible</h2>
+                <h2 className='text-center font-extrabold text-white uppercase text-5xl'>Factoraje sostenible</h2>
 
-            <p className='font-extrabold text-center text-black text-2xl uppercase'>conoce los beneficios</p>
+                <p className='font-extrabold text-center text-black text-2xl uppercase'>conoce los beneficios</p>
 
-            <div className='flex justify-center gap-5 '>
-              <div><p className='py-3 pr-2 border-r-4 border-slate-300 text-sm font-bold uppercase'>requisitos</p></div>
-              <div><p className='py-3 pr-2 border-r-4 border-slate-300 text-sm font-bold uppercase'>beneficios en la taza</p></div>
-              <div><p className='py-3 pr-2 border-r-4 border-slate-300 text-sm font-bold uppercase'>empresa sustentable</p></div>
-            </div>
+                <div className='flex justify-center gap-5 '>
+                  <div><p className='py-3 pr-2 border-r-4 border-slate-300 text-sm font-bold uppercase'>requisitos</p></div>
+                  <div><p className='py-3 pr-2 border-r-4 border-slate-300 text-sm font-bold uppercase'>beneficios en la taza</p></div>
+                  <div><p className='py-3 pr-2 border-r-4 border-slate-300 text-sm font-bold uppercase'>empresa sustentable</p></div>
+                </div>
+
+              </div>
+
+              <div className=' relative '>
+                <Video/>
+              </div>
 
           </div>
 
-          <div className=' relative '>
-            <Video/>
-          </div>
-
-      </div>
+          
 
           <div className="slide ">
 
@@ -82,6 +86,26 @@ const [currentSlide, setCurrentSlide] = useState(0);
 
             <img className='w-full h-full object-cover' src={presentacion}  alt="" />
   
+          </div>
+
+          <div className="slide relative ">
+
+            <ReactPlayer
+              height="100%"
+              width="100%"
+              playing={false}
+              controls={true}
+              loop={true}
+              muted={true}
+              url="https://www.youtube.com/watch?v=hstDHZRqPe0"
+              style={{ 
+                position: "absolute",
+                top: "0",
+                left:"0",
+                objectFit: "cover",
+              }}
+            />
+              
           </div>
 
         </div>
